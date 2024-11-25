01511319f3ff2f7fc7128bf83fe81fefadc4d910
Muqtada mushtaq abbas <115376448+cv1awaw@users.noreply.github.com>
2024-11-25 23:03:02 +0300

    Update roles.py

--- roles.py
+++ roles.py
@@ -1,7 +1,7 @@
 # roles.py
 
 WRITER_IDS = {6177929931, 987654321}  # Replace with actual writer user IDs
-MCQS_TEAM_IDS = {6177929931, 345678901}  # Replace with actual MCQs team user IDs
+MCQS_TEAM_IDS = {767u, 345678901}  # Replace with actual MCQs team user IDs
 CHECKER_TEAM_IDS = {6177929931 , 567890123}  # Replace with actual checker team user IDs
 WORD_TEAM_IDS = {678901234, 789012345}  # Replace with actual word team user IDs
 DESIGN_TEAM_IDS = {890123456, 901234567}  # Replace with actual design team user IDs
