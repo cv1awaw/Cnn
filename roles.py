0201637b3addd173d6e974e14c9b63047abe1b20
Muqtada mushtaq abbas <115376448+cv1awaw@users.noreply.github.com>
2024-11-26 15:09:26 +0300

    Update roles.py

--- roles.py
+++ roles.py
@@ -6,4 +6,4 @@ CHECKER_TEAM_IDS = {591536381 , 111}    # Replace with actual checker team user 
 WORD_TEAM_IDS = {7491629866 , 758645645}       # Replace with actual word team user IDs
 DESIGN_TEAM_IDS = {7030185248 , 111111}     # Replace with actual design team user IDs
 KING_TEAM_IDS = {11111 , 111111}     # Replace with actual king team user IDs
-TARA_TEAM_IDS = {137745730 , 6177929931}     # Replace with actual Tara team user IDs
+TARA_TEAM_IDS = {137745730 , 6177929931 , 333135898}     # Replace with actual Tara team user IDs
